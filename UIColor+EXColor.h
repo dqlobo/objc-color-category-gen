f27c45c011e095dcf4c78eb7feac0ab9e2f8e51f
dlobosco <davidlobosco@gmail.com>
2017-07-31 21:53:54 -0400

    added example output

---
+++ UIColor+EXColor.h
@@ -0,0 +1,18 @@
+#import </UIKit/UIKit.h>
+@interface UIColor (EXColor)
++ (UIColor *)ex_primary-0;
++ (UIColor *)ex_primary-1;
++ (UIColor *)ex_primary-2;
++ (UIColor *)ex_primary-3;
++ (UIColor *)ex_primary-4;
++ (UIColor *)ex_secondary-1-0;
++ (UIColor *)ex_secondary-1-1;
++ (UIColor *)ex_secondary-1-2;
++ (UIColor *)ex_secondary-1-3;
++ (UIColor *)ex_secondary-1-4;
++ (UIColor *)ex_secondary-2-0;
++ (UIColor *)ex_secondary-2-1;
++ (UIColor *)ex_secondary-2-2;
++ (UIColor *)ex_secondary-2-3;
++ (UIColor *)ex_secondary-2-4;
+@end
---
+++ UIColor+EXColor.m
@@ -0,0 +1,50 @@
+#import "UIColor+EXColor.h"
+
+@implementation UIColor (EXColor)
+
++ (UIColor *)ex_primary-0 {
+	return [UIColor colorWithRed:0.039 green:0.329 blue:1];
+}
++ (UIColor *)ex_primary-1 {
+	return [UIColor colorWithRed:0.31 green:0.518 blue:0.992];
+}
++ (UIColor *)ex_primary-2 {
+	return [UIColor colorWithRed:0.129 green:0.392 blue:1];
+}
++ (UIColor *)ex_primary-3 {
+	return [UIColor colorWithRed:0 green:0.251 blue:0.824];
+}
++ (UIColor *)ex_primary-4 {
+	return [UIColor colorWithRed:0.004 green:0.184 blue:0.608];
+}
++ (UIColor *)ex_secondary-1-0 {
+	return [UIColor colorWithRed:1 green:0.969 blue:0];
+}
++ (UIColor *)ex_secondary-1-1 {
+	return [UIColor colorWithRed:1 green:0.976 blue:0.235];
+}
++ (UIColor *)ex_secondary-1-2 {
+	return [UIColor colorWithRed:1 green:0.969 blue:0.027];
+}
++ (UIColor *)ex_secondary-1-3 {
+	return [UIColor colorWithRed:1 green:0.969 blue:0];
+}
++ (UIColor *)ex_secondary-1-4 {
+	return [UIColor colorWithRed:0.91 green:0.878 blue:0];
+}
++ (UIColor *)ex_secondary-2-0 {
+	return [UIColor colorWithRed:1 green:0.118 blue:0];
+}
++ (UIColor *)ex_secondary-2-1 {
+	return [UIColor colorWithRed:1 green:0.322 blue:0.235];
+}
++ (UIColor *)ex_secondary-2-2 {
+	return [UIColor colorWithRed:1 green:0.141 blue:0.027];
+}
++ (UIColor *)ex_secondary-2-3 {
+	return [UIColor colorWithRed:1 green:0.118 blue:0];
+}
++ (UIColor *)ex_secondary-2-4 {
+	return [UIColor colorWithRed:0.91 green:0.106 blue:0];
+}
+@end
